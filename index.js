0886d6902f87aefe77b0e6123abcc25c413f1fbd
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-08 22:07:01 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const mainNode = document.querySelector('main#main');
+    if (mainNode) {
+      mainNode.remove();
+    }
+    const newHeader = document.createElement('h1');
+    console.log (document.createElement)
+    newHeader.id = 'victory';
+    newHeader.innerHTML = "YOUR-NAME is the champion"; 
+    document.body.appendChild(newHeader);
+    
+});
